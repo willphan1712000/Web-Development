2ae734cae0706997a3d04182be727d613cba5a3f
Thanh Nha Phan <85920204+willphan1712000@users.noreply.github.com>
2023-12-06 15:31:26 -0500

    Update fileManipulation.php

--- server/fileManipulation.php
+++ server/fileManipulation.php
@@ -1,20 +1,20 @@
 <?php
   function deleteFolder($path) {
-        if(!is_dir($path)) {
-            return false;
-        }
+      if(!is_dir($path)) {
+          return false;
+      }
 
-        $files = array_diff(scandir($path), array('.', '..'));
+      $files = array_diff(scandir($path), array('.', '..'));
 
-        foreach($files as $file) {
-            $filePath = $path.'/'.$file;
-            if(is_dir($filePath)) {
-                deleteFolder($filePath);
-            } else {
-                unlink($filePath);
-            }
-        }
+      foreach($files as $file) {
+          $filePath = $path.'/'.$file;
+          if(is_dir($filePath)) {
+              deleteFolder($filePath);
+          } else {
+              unlink($filePath);
+          }
+      }
 
-        return rmdir($path);
-    }
+      return rmdir($path);
+  }
 ?>
