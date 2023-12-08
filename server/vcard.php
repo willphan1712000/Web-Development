9ac145a565605c3c121b825af4a6f173a435f07c
Thanh Nha Phan <85920204+willphan1712000@users.noreply.github.com>
2023-12-08 09:50:05 -0500

    Update vcard.php

--- server/vcard.php
+++ server/vcard.php
@@ -1,34 +1,47 @@
 <?php
-	include_once "image.php"; // This is where you get the base64 encoded image
-	// Define vCard data
-	$name = 'Phan Thanh Nha';
-	$email = 'john.doe@example.com';
-	$phone = '+1234567890';
-	$facebook = 'https://www.facebook.com/johndoe';
-	// Build vCard content
-	$vCardContent = "BEGIN:VCARD
-	VERSION:3.0
-	REV:2023-12-08T06:00:48Z
-	URL:https://danhbaso.net/example/
-	NOTE;CHARSET=utf-8:🌺 Cuộc sống chỉ có thể được nhìn ngược về quá khứ; nhưng
-	nó phải được sống hướng về phía trước 💜
-	N;CHARSET=utf-8:$name;;;;
-	FN;CHARSET=utf-8:$name
-	TEL;PREF:$phone
-	EMAIL;TYPE=Email:$email
-	URL;TYPE=Facebook:$facebook
-	URL;TYPE=TikTok:https://tiktok.com/@tiktok
-	URL;TYPE=Facebook messenger:https://m.me/chatbot
-	URL;TYPE=Zalo:https://zalo.me/0999999999
-	URL;TYPE=Số tài khoản ngân hàng:1903483483
-	PHOTO;ENCODING=b;TYPE=JPEG:$imageData
-	END:VCARD
-	";
+    include "conn.php";
+    if(isset($_POST['vcard'])) {
+        $username = $_POST['vcard'];
+        $infoQuery = mysqli_query($conn, "SELECT *FROM info WHERE username = '$username'");
+        $infoArray = mysqli_fetch_assoc($infoQuery);
 
-	// Set headers to trigger a download
-	header('Content-type: text/vcard');
-	header('Content-Disposition: attachment; filename="contact.vcf"');
-
-	// Output the vCard content
-	echo $vCardContent;
-?>
+        $img = $infoArray['image'];
+        $name = $infoArray['name'];
+        $des = $infoArray['des'];
+        $Phone = $infoArray['Phone'];
+        $Email = $infoArray['Email'];
+        $Map = $infoArray['Map'];
+        $Facebook = $infoArray['Facebook'];
+        $Instagram = $infoArray['Instagram'];
+        $Messenger = $infoArray['Messenger'];
+        $Zalo = $infoArray['Zalo'];
+        $Tiktok = $infoArray['Tiktok'];
+        if(!empty($img)) {
+            $imageData = base64_encode(file_get_contents("../".$username."/".$img));
+        }
+    
+        $vCardContent = "BEGIN:VCARD
+        VERSION:3.0
+        REV:2023-12-08T06:00:48Z
+        NOTE;CHARSET=utf-8:$des
+        N;CHARSET=utf-8:$name;;;;
+        FN;CHARSET=utf-8:$name
+        TEL;PREF:$Phone
+        EMAIL;TYPE=Email:$Email
+        URL;TYPE=Map:$Map
+        URL;TYPE=Facebook:$Facebook
+        URL;TYPE=Messenger:$Messenger
+        URL;TYPE=Instagram:$Instagram
+        URL;TYPE=TikTok:$Tiktok
+        URL;TYPE=Zalo:$Zalo
+        PHOTO;ENCODING=b;TYPE=JPEG:$imageData
+        END:VCARD
+        ";
+    
+        // Set headers to trigger a download
+        header('Content-type: text/vcard');
+        header('Content-Disposition: attachment; filename="contact.vcf"');
+    
+        // Output the vCard content
+        echo $vCardContent;
+    }
